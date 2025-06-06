85a87a8e448b303e746fff9f3aada82beccd2e2d
Tallie <84561047+Friendly-Neighbourhood-Insomniac@users.noreply.github.com>
2025-06-06 16:45:09 +0200

    Fix Hiber3D Vite plugin import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { hiber3DVitePlugin } from "@hiber3d/web/vite-plugin";
+import { hiber3DVitePlugin } from "@hiber3d/web";
 
 // https://vite.dev/config/
 export default defineConfig({
